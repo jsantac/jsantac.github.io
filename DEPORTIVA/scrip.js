8b43478822b6d7647fb9cdd55ddf89d70c4135a9
jsantac <112987283+jsantac@users.noreply.github.com>
2022-10-01 12:26:09 -0300

    Add files via upload

---
+++ DEPORTIVA/scrip.js
@@ -0,0 +1,12 @@
+let edad;
+edad = prompt("Introduce tu edad");
+if (edad >= 18){
+alert("Puedes entrar, eres mayor de edad.");
+} else {
+alert("No puedes entrar, eres menor de edad.");
+}
+
+function cerrar() {
+    window.close();
+}
+
